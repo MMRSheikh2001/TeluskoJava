d17c7829f0e596a7408c87ac0af0c98e3da38c03
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-15 00:27:58 +0600

    fgf

--- CoreJava/DataTypess/src/App.java
+++ CoreJava/DataTypess/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        boolean a = false;
+        System.out.println(!a);
+        if (a == true) {
+            System.out.println("Me");
+        }
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        sc.close();
+        System.out.println(n);
+        char ap='u';
+        System.out.println(ap);
+
     }
 }
