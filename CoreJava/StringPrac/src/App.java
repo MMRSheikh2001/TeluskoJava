545c2d64439c3213ff0dc46f5f358e7f3ff32c48
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-16 00:05:51 +0600

    xz

--- CoreJava/StringPrac/src/App.java
+++ CoreJava/StringPrac/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int i = 4;
+        int result = ++i;
+        result = i++;
+        System.out.println(result);
+        System.out.println(result == i);
     }
 }
